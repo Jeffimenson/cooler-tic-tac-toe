633010732c9ba84c6682b8c5168a8d9b3582b7d5
Jeffimenson <jeffengim@gmail.com>
2023-07-24 11:56:05 -0400

    Reorganize dom generating code into public module function and prepare for future functions

--- main.js
+++ main.js
@@ -1,47 +1,61 @@
 const display = (function(){
     const body = document.querySelector('body');
-    const bigGrid = document.createElement('div');
-    bigGrid.classList.add('big-grid');
-    body.appendChild(bigGrid);
+    const bigGrid = {
+        grid: document.createElement('div'),
+        miniGrids: [
+            new Array(3),
+            new Array(3),
+            new Array(3)
+        ]
+    }; 
 
-    const miniGrids = [
-        new Array(3),
-        new Array(3),
-        new Array(3)
-    ];
-    const gridLength = 3;
-    for (let i = 0; i < gridLength; i++){
-        for (let j = 0; j < gridLength; j++){
-            const miniGrid = document.createElement('div');
-            miniGrid.classList.add('mini-grid');
-            bigGrid.appendChild(miniGrid);
+    function generateDomElements(){
+        bigGrid.grid.classList.add('big-grid');
+        body.appendChild(bigGrid.grid);
 
-            const miniCells = [
-                new Array(3),
-                new Array(3),
-                new Array(3)
-            ];
+        const gridLength = 3;
+        for (let i = 0; i < gridLength; i++){
+            for (let j = 0; j < gridLength; j++){
+                const miniGrid = document.createElement('div');
+                miniGrid.classList.add('mini-grid');
+                bigGrid.grid.appendChild(miniGrid);
 
-            for (let i = 0; i < gridLength; i++){
-                for (let j = 0; j < gridLength; j++){
-                    const cell = document.createElement('div');
-                    cell.classList.add('mini-cell');
-                    
-                    miniGrid.appendChild(cell);
+                const miniCells = [
+                    new Array(3),
+                    new Array(3),
+                    new Array(3)
+                ];
 
-                    miniCells[i][j] = cell;
+                for (let i = 0; i < gridLength; i++){
+                    for (let j = 0; j < gridLength; j++){
+                        const cell = document.createElement('button');
+                        cell.setAttribute('type', 'button');
+                        cell.classList.add('mini-cell');
+                        cell.addEventListener('click', _onCellClick);
+                        
+                        miniGrid.appendChild(cell);
+                        miniCells[i][j] = cell;
+                    }
                 }
+                bigGrid.miniGrids[i][j] = {
+                    grid: miniGrid,
+                    cells: miniCells 
+                };
             }
-            miniGrids[i][j] = {
-                grid: miniGrid,
-                cells: miniCells 
-            };
-            
         }
+    } 
+
+    function _onCellClick(){
+        // WORK IN PROGRESS
     }
-    
+
+    return {
+        generateDomElements
+    };
 })();
 
 const game = (function(){
     
 })();
+
+display.generateDomElements();
