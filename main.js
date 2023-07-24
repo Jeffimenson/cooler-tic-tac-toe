4bdb8f9220bf7dad0d3f57cab0c2da9379503dcb
Jeffimenson <jeffengim@gmail.com>
2023-07-24 16:43:05 -0400

    Add makeMark function and make game module return stuff

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
 const display = (function(){
-    const body = document.querySelector('body');
-    const bigGrid = {
-        grid: document.createElement('div'),
-        miniGrids: [
+    const _body = document.querySelector('body');
+    const _bigGrid = {
+        grid: null,
+        miniGrids: null
+    }; 
+
+    const gridLength = 3;
+    function generateDomElements(){ //I might refactor this function in the future so it can be used to clear the grids and make new ones for a new game
+         _bigGrid.grid = document.createElement('div');
+         _bigGrid.miniGrids = [
             new Array(3),
             new Array(3),
             new Array(3)
-        ]
-    }; 
+        ];       
+        _bigGrid.grid.classList.add('big-grid');
+        _body.appendChild(_bigGrid.grid);
 
-    function generateDomElements(){
-        bigGrid.grid.classList.add('big-grid');
-        body.appendChild(bigGrid.grid);
-
-        const gridLength = 3;
         for (let i = 0; i < gridLength; i++){
             for (let j = 0; j < gridLength; j++){
-                const miniGrid = document.createElement('div');
-                miniGrid.classList.add('mini-grid');
-                bigGrid.grid.appendChild(miniGrid);
-
-                const miniCells = [
-                    new Array(3),
-                    new Array(3),
-                    new Array(3)
-                ];
-
-                for (let i = 0; i < gridLength; i++){
-                    for (let j = 0; j < gridLength; j++){
-                        const cell = document.createElement('button');
-                        cell.setAttribute('type', 'button');
-                        cell.classList.add('mini-cell');
-                        cell.addEventListener('click', _onCellClick);
-                        
-                        miniGrid.appendChild(cell);
-                        miniCells[i][j] = cell;
-                    }
-                }
-                bigGrid.miniGrids[i][j] = {
-                    grid: miniGrid,
-                    cells: miniCells 
-                };
+                _bigGrid.miniGrids[i][j] = _MiniGrid();
+                _bigGrid.grid.appendChild(_bigGrid.miniGrids[i][j].grid);
             }
         }
     } 
+
+    function _MiniGrid(){
+        const miniGrid = document.createElement('div');
+        miniGrid.classList.add('mini-grid');
+
+        const miniCells = [
+            new Array(3),
+            new Array(3),
+            new Array(3)
+        ];
+
+        for (let r = 0; r < gridLength; r++){
+            for (let c = 0; c < gridLength; c++){
+                const cell = document.createElement('button');
+                cell.setAttribute('type', 'button');
+                cell.classList.add('mini-cell');
+                cell.addEventListener('click', _onCellClick);
+                miniGrid.appendChild(cell);
+                miniCells[r][c] = cell;
+            }
+        }
+        return {
+            grid: miniGrid,
+            cells: miniCells 
+        }
+    }
 
     function _onCellClick(){
         // WORK IN PROGRESS
@@ -55,7 +60,53 @@ const display = (function(){
 })();
 
 const game = (function(){
-    
+    const _bigGrid = [
+        new Array(3),
+        new Array(3),
+        new Array(3)
+    ];
+    let nextMark = 'X';
+
+    const gridLength = 3;
+    function newGame(){
+        for (let r = 0; r < gridLength; r++){
+            for (let c = 0; c < gridLength; c++){
+                _bigGrid[r][c] = _MiniGrid(); 
+            }
+        }
+    }
+
+    function makeMark(bigCoord, miniCoord){
+        const [bigR, bigC] = bigCoord;
+        const [miniR, miniC] = miniCoord;
+        
+        _bigGrid[bigR][bigC].grid[miniR][miniC] = nextMark;
+        _changeNextMark();
+        console.log(_bigGrid);
+    }
+
+    function _changeNextMark(){
+        nextMark = (nextMark === 'X') ? 'O' : 'X';
+    }
+
+    function _MiniGrid(){
+        return {
+            takenBy: null,
+            grid: [
+                new Array(3),
+                new Array(3),
+                new Array(3)
+            ]
+        }
+    }
+
+    return {
+        newGame,
+        makeMark
+    }
+
 })();
+
+game.newGame();
 
 display.generateDomElements();
